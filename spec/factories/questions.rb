36a4452b44b9bfaf5be145cb671669b7fb30c205
Alexey Orloff <alex.yourloff@gmail.com>
2022-07-19 12:12:51 +0700

    init

---
+++ spec/factories/questions.rb
@@ -0,0 +1,14 @@
+# http://www.rubydoc.info/gems/factory_girl/file/GETTING_STARTED.md
+
+FactoryGirl.define do
+  factory :question do
+    answer1 { "#{rand(2001)}" }
+    answer2 { "#{rand(2001)}" }
+    answer3 { "#{rand(2001)}" }
+    answer4 { "#{rand(2001)}" }
+
+    sequence(:text) { |n| "В каком году была космическая одиссея #{n}?" }
+
+    sequence(:level) { |n| n % 15 }
+  end
+end
